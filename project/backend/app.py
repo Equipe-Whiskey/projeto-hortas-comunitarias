c69e3bb20bebb3856030fe3f215e588de012f64a
Lvii <blueice5700@gmail.com>
2024-10-07 19:22:33 -0300

    atualizado backend e frontend
    tive q fazer isso rapido pra ganhar aquela BATATA FRITA GRATIS DO BURGER KING

--- project/backend/app.py
+++ project/backend/app.py
@@ -15,5 +15,16 @@ def add_project():
     projects.append(new_project)
     return jsonify(new_project), 201
 
+@app.route('/projects/<int:index>', methods=['PUT'])
+def edit_project(index):   
+    edited_data = request.get_json()
+    projects[index].update(edited_data)
+    return jsonify(projects[index]), 200
+
+@app.route('/projects/<int:index>', methods=['DELETE'])
+def delete_project(index):
+    deleted_project = projects.pop(index)
+    return jsonify(deleted_project), 200
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
--- project/frontend/src/App.js
+++ project/frontend/src/App.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 
 function App() {
     const [projects, setProjects] = useState([]); // Estado para armazenar os projetos
-    const [selectedProject, setSelectedProject] = useState(-1);
+    const [editMode, setEditMode] = useState(false); // Estado do modo de edição
+    const [selectedProject, setSelectedProject] = useState(-1); // Estado para mostrar os detalhes do projeto selecionado
 
     // useEffect é chamado quando o componente é montado
     useEffect(() => {
@@ -20,8 +21,11 @@ function App() {
         <div>
             <h1>Hortas Comunitárias da Equipe Whiskey</h1>
 
-            <h2>Formulário de Criação de Projetos</h2>
-            <Form />
+            {editMode ? (
+                <EditForm />
+            ):(
+                <SubmitForm />
+            )}
             <p>-----------------------------------------</p>
 
             {/* Exibindo os projetos apenas se houver algum */}
@@ -30,36 +34,40 @@ function App() {
                 <p>Nenhum projeto foi adicionado ainda.</p>
             ) : (
                 <div>
+
+                    {selectedProject >= 0 ? (
+                        <div>
+                            <h2>{projects[selectedProject].nome}</h2>
+                            <strong>Descrição:</strong> {projects[selectedProject].descricao} <br />
+                            <strong>Status:</strong> {projects[selectedProject].status ? "Completo" : "Incompleto"}
+                            <p onClick={() => setEditMode(true)}>Editar Projeto</p>
+                            <p onClick={DeleteForm()}>Deletar Projeto</p>
+                        </div>
+                    ) : (
+                        <p>Selecione um projeto para ver seus detalhes!</p>
+                    )}
+
+                    <p>-----------------------------------------</p>
+
                     <ul>
                         {projects.map((project, index) => (
-                            <div><li key={index} onClick={() => setSelectedProject(index)}>
+                            <div><li onClick={() => setSelectedProject(index)}>
                                 <strong>Nome:</strong> {project.nome} <br />
                                 <strong>Descrição:</strong> {project.descricao}
                             </li><br /></div>
                         ))}
                     </ul>
 
-                    <p>-----------------------------------------</p>
-
-                    {selectedProject >= 0 ? (
-                        <div>
-                            <h2>{projects[selectedProject].nome}</h2> <br />
-                            <strong>Descrição:</strong> {projects[selectedProject].descricao} <br />
-                            <strong>Status:</strong> {projects[selectedProject].status ? "Completo" : "Incompleto"}
-                        </div>
-                    ) : (
-                        <p>Selecione um projeto para ver seus detalhes!</p>
-                    )}
                 </div>
             )}
 
-            
         </div>
     );
 
-    function Form() {
+    // Formulário para criar novos projetos
+    function SubmitForm() {
 
-        const [inputs, setInputs] = useState({nome:"", descricao:"", status:false});
+        const [inputs, setInputs] = useState({ nome: "", descricao: "", status: false });
 
         const handleChange = (event) => {
             const name = event.target.name;
@@ -77,12 +85,12 @@ function App() {
         }
 
         const handleSubmit = async (event) => {
-            event.preventDefault();
+            event.preventDefaults();
 
             try {
                 const response = await axios.post('http://localhost:5000/projects', inputs);
                 alert(JSON.stringify(inputs) + "\nProjeto enviado com sucesso!");
-                setProjects(prevProjects => [inputs, ...prevProjects]);
+                setProjects(updateProjects => [...updateProjects, inputs]);
                 setInputs({});
             } catch (error) {
                 console.error("", error);
@@ -92,48 +100,162 @@ function App() {
         }
 
         return (
-            <form onSubmit={handleSubmit}>
-                <label>Nome do projeto:
-                    <input
-                        type="text"
-                        name="nome"
-                        value={inputs.nome || ""}
-                        onChange={handleChange}
-                    /> <br />
-                </label>
+            <div>
+                <h2>Formulário de Criação de Projetos</h2>
+                <form onSubmit={handleSubmit}>
+                    <label>Nome do projeto:
+                        <input
+                            type="text"
+                            name="nome"
+                            value={inputs.nome || ""}
+                            onChange={handleChange}
+                        /> <br />
+                    </label>
 
-                <label>Descrição:
-                    <br />
-                    <textarea
-                        type="text"
-                        name="descricao"
-                        value={inputs.descricao || ""}
-                        onChange={handleChange}
-                        rows="4"
-                        cols="60"
-                    /> <br />
-                </label>
+                    <label>Descrição:
+                        <br />
+                        <textarea
+                            type="text"
+                            name="descricao"
+                            value={inputs.descricao || ""}
+                            onChange={handleChange}
+                            rows="4"
+                            cols="60"
+                        /> <br />
+                    </label>
 
-                <label>Completo?
-                    <input
-                        type="checkbox"
-                        name="status"
-                        checked={inputs.status || false}
-                        onChange={handleChange}
-                    /> <br />
-                </label>
-                
-                {/* Apenas exibe o botão de envio caso o projeto a ser enviado tenha nome e descrição*/}
-                {
-                JSON.stringify(inputs.nome).length >= 3 && JSON.stringify(inputs.descricao).length >= 3 ? 
-                <div>
-                    <br /><input type="submit" value="Criar" />
-                </div>
-                :
-                <p style={{color:"#f00"}}>Insira um título e uma descrição para criar um projeto!</p>
-                }
-            </form>
+                    <label>Completo?
+                        <input
+                            type="checkbox"
+                            name="status"
+                            checked={inputs.status || false}
+                            onChange={handleChange}
+                        /> <br />
+                    </label>
+
+                    {/* Apenas exibe o botão de envio caso o projeto a ser enviado tenha nome e descrição*/}
+                    {
+                        JSON.stringify(inputs.nome).length >= 3 && JSON.stringify(inputs.descricao).length >= 3 ?
+                            <div>
+                                <br /><input type="submit" value="Criar" />
+                            </div>
+                            :
+                            <p style={{ color: "#f00" }}>Insira um título e uma descrição para criar um projeto!</p>
+                    }
+                </form>
+            </div>
         )
+    }
+
+    // Formulário para editar projetos existentes
+    function EditForm() {
+
+        const [inputs, setInputs] = useState(projects[selectedProject]);
+
+        const handleChange = (event) => {
+            const name = event.target.name;
+            let value;
+
+            switch (event.target.type) {
+                case 'checkbox':
+                    value = event.target.checked;
+                    break;
+                default:
+                    value = event.target.value;
+                    break;
+            }
+            setInputs(values => ({ ...values, [name]: value }))
+        }
+
+        const handleSubmit = async (event) => {
+            event.preventDefaults();
+
+            try {
+                const response = await axios.put('http://localhost:5000/projects/'+selectedProject, inputs);
+                alert(JSON.stringify(inputs) + "\nProjeto editado com sucesso!");
+                setEditMode(false);
+                setInputs({});
+
+                // Atualiza a lista de projetos, vendo se 
+                setProjects(updateProjects => updateProjects.map((project, index) => 
+                    index === selectedProject ? 
+                        updateProjects[index] = inputs
+                     : 
+                        project
+                    )
+                );
+            } catch (error) {
+                console.error("", error);
+                alert("Houve um erro ao editar os dados.\n" + error);
+            }
+
+        }
+
+
+        return (
+            <div>
+                <h2 style={{ color: "#0a0" }}>Formulário de Edição de Projetos</h2>
+                <form onSubmit={handleSubmit}>
+                    <label>Nome do projeto:
+                        <input
+                            type="text"
+                            name="nome"
+                            value={inputs.nome || ""}
+                            onChange={handleChange}
+                        /> <br />
+                    </label>
+
+                    <label>Descrição:
+                        <br />
+                        <textarea
+                            type="text"
+                            name="descricao"
+                            value={inputs.descricao || ""}
+                            onChange={handleChange}
+                            rows="4"
+                            cols="60"
+                        /> <br />
+                    </label>
+
+                    <label>Completo?
+                        <input
+                            type="checkbox"
+                            name="status"
+                            checked={inputs.status || false}
+                            onChange={handleChange}
+                        /> <br />
+                    </label>
+
+                    {/* Apenas exibe o botão de envio caso o projeto a ser enviado tenha nome e descrição*/}
+                    {
+                        JSON.stringify(inputs.nome).length >= 3 && JSON.stringify(inputs.descricao).length >= 3 ?
+                            <div>
+                                <br /><input type="submit" value="Editar" />
+                            </div>
+                            :
+                            <p style={{ color: "#f00" }}>Insira um título e uma descrição para editar este projeto!</p>
+                    }
+                </form>
+            </div>
+        )
+    }
+
+    function DeleteForm() {
+        return async (event) => {
+            try {
+                const response = await axios.delete('http://localhost:5000/projects/'+selectedProject);
+                alert("Projeto '" + projects[selectedProject].nome + "' deletado com sucesso!");
+
+                // Remove o projeto deletado da lista e sai do modo de edição
+                                                        // Esse "_" representa todos os projetos individuais
+                setProjects(updateProjects => updateProjects.filter((_, index) => index !== selectedProject));
+                setEditMode(false);
+                setSelectedProject(-1);
+            } catch (error) {
+                console.error("Houve um erro ao deletar o projeto: " , error);
+                alert("Houve um erro ao deletar o projeto: " + error);
+            }
+        }
     }
 }
 export default App;
